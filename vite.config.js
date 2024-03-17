b42601b32f1d144636baa432423222e292d0cc83
Lemuel <lemuelcevallos90@gmail.com>
2024-03-16 21:39:40 -0600

    Agregar Base de deploy

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import handlerBarsContext from './variables.js';
 
 export default defineConfig({
-    base: "/",
+    base: "/Portafolio/",
     appType: 'mpa',
     build: {
         rollupOptions: {
